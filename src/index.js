8772f7c3c8d7f7770da9fc820fe621b6da452cfe
Dmitry Mikhailyuk <grinpigrin@gmail.com>
2021-08-20 12:32:35 +0300

    JSX

--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Header = () => {
+  return <h2>Hello world</h2> 
+}
+
+const Field = () => {
+  const holder = "Enter here"
+  const styledField  = {
+    width: '300px'
+  }
+
+  return <input 
+            style={styledField}
+            type="text" 
+            placeholder={holder} 
+            name="" 
+            id=""
+            autoComplete = ""
+            className = "uups"
+            htmlFor = "" />
+}
+
+const Btn = () => {
+  const text = "Log in"
+  const logged = true;
+  return <button>{logged ? "Enter" : text}</button>
+}
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Field/>
+      <Btn/>
+    </div>
+  )
+}
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
 
