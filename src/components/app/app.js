7185902591f464cbc0377348e938a1cde1407bb2
Dmitry Mikhailyuk <grinpigrin@gmail.com>
2021-08-21 10:50:03 +0300

    props and state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,13 @@ import PostAddForm from "../post-add-form/post-add-form"
 import './app.css'
 
 const App = () => {
+
+    const data = [ 
+        {label: 'Check my Github', important: true, id: 'sdfsdgs'},
+        {label: 'Wow! Codewars is unbelievable', important: false, id: 'dsfsdg'},
+        {label: 'Please fuck me!', important: false, id: 'dsfsdaaa'},
+    ];
+
     return (
         <div className="app">
             <AppHeader/>
@@ -16,7 +23,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     )
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,18 +1,55 @@
-import React from "react"
+import React, {Component} from "react"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar, faTrash, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 import './post-list-item.css'
 
-const PostListItem = () => {
-    return (
-        <li className="app-list-item d-flex justify-content-between">
-        <span className="app-list-item-label">
-            Hello World
-        </span>
+export default class PostListItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            important: false,
+            like: false
+        }
+        this.onImportant = this.onImportant.bind(this);
+        this.onLike = this.onLike.bind(this);
+    }
+
+    onImportant() {
+        this.setState(({important}) => ({
+            important: !important
+        }))
+    }
+
+    onLike() {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
+
+    render() {
+        const {label} = this.props
+        const {important, like} = this.state
+        let classNames = 'app-list-item d-flex justify-content-between'
+        
+        if (important) {
+            classNames += ' important'
+        }
+
+        if (like) {
+            classNames += ' like'
+        }
+
+        return (
+        <div className={classNames}>
+            <span 
+            className="app-list-item-label"
+            onClick={this.onLike}>
+                {label}
+            </span>
         <div className="d-flex justify-content-center align-items-center">
-            <button className="btn-star btn-sm" type="submit">
+            <button className="btn-star btn-sm" type="submit" onClick={this.onImportant}>
                 <FontAwesomeIcon icon={faStar} />
             </button>
             <button className="btn-trash btn-sm" type="submit">
@@ -20,8 +57,7 @@ const PostListItem = () => {
             </button>
                 <FontAwesomeIcon icon={faHeart} />
         </div>
-    </li>
-    )
+        </div>
+        )
+    }
 }
-
-export default PostListItem
